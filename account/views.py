907b5c115c19e939034bfd928eb3cf19be4d2d38
Aditya Vikram Singh <adivik672@gmail.com>
2024-06-02 19:27:05 +0530

    Message framework added

--- account/views.py
+++ account/views.py
@@ -9,6 +9,7 @@ from .forms import (
 )
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 
 def user_login(request):
@@ -74,6 +75,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Profile updated successfully")
+        else:
+            messages.error(request, "Error updating your profile")
 
     else:
         user_form = UserEditForm(instance=request.user)

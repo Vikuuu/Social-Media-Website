510edddb54eddf1a4a369c615b9c7d7902899518
Aditya Vikram Singh <adivik672@gmail.com>
2024-06-03 22:25:48 +0530

    Image app created

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config # type: ignore
+from decouple import config  # type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
 INSTALLED_APPS = [
     # Local apps
     "account.apps.AccountConfig",
+    "image.apps.ImageConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

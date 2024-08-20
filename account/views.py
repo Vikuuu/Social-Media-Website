b4511821138ba7896d43f664d1f7ea7a596784b6
Aditya Vikram Singh <adivik672@gmail.com>
2024-08-20 19:00:19 +0530

    Added user follow/unfollow action with JS.

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login, get_user_model
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import (
     LoginForm,
     UserRegistrationForm,
@@ -8,8 +8,9 @@ from .forms import (
     ProfileEditForm,
 )
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile, Contact
 from django.contrib import messages
+from django.views.decorators.http import require_POST
 
 
 def user_login(request):
@@ -105,3 +106,27 @@ def user_detail(request, username):
     user = get_object_or_404(User, username=username, is_active=True)
     context = {"section": "people", "user": user}
     return render(request, "account/user/detail.html", context)
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == "follow":
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user,
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user,
+                    user_to=user,
+                ).delete()
+            return JsonResponse({"status": "ok"})
+        except User.DoesNotExist:
+            return JsonResponse({"status": "error"})
+    return JsonResponse({"status": "error"})
